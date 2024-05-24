41bea63cb731afff27a62187f1c19c49a5a42650
Matthew Dudak <compassman495@gmail.com>
2024-05-24 08:03:58 -0500

    Made it prettier, and added variable names.

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+function threeSigFigs(num) {
+    return num.toPrecision(3)
+}
+
+
 function generateData(ev) {
+    const ind_var_name = document.querySelector("#ind-name").value
+    document.querySelector("#ind-name-table").textContent = ind_var_name
+    document.querySelector("#ind-name-latex").textContent = ind_var_name
+    const dep_var_name = document.querySelector("#dep-name").value
+    document.querySelector("#dep-name-table").textContent = dep_var_name
+    document.querySelector("#dep-name-latex").textContent = dep_var_name
     const tbody = document.querySelector("#data-body")
     const template = document.querySelector("#data-row")
     const latex_body = document.querySelector("#latex-code > tbody")
@@ -28,12 +39,12 @@ function generateData(ev) {
         deps[i] = eqn.evaluate(scope) + ((dep_range/num_obs)*(Math.random()-0.5)*error/100)//NEED RANDOMIZER
         const clone = template.content.cloneNode(true)
         let td = clone.querySelectorAll("td")
-        td[0].textContent = inds[i]
-        td[1].textContent = deps[i]
+        td[0].textContent = threeSigFigs(inds[i])
+        td[1].textContent = threeSigFigs(deps[i])
         tbody.appendChild(clone)
         const latex_clone = latex_template.content.cloneNode(true)
         let latex_td = latex_clone.querySelector("td")
-        latex_td.textContent = inds[i] + "&" + deps[i] + "\\\\"
+        latex_td.textContent = threeSigFigs(inds[i]) + "&" + threeSigFigs(deps[i]) + "\\\\"
         latex_body.insertBefore(latex_clone, latex_before)
     }
     console.log(deps)
